ae0b447c06a7d8efb8c9e3711cb57567be701486
ohm <ohmkunn@hotmail.com>
2023-02-20 15:07:45 +0700

    asm 6

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cookieParser=require('cookie-parser');
 const connectDB = require('./config/db');
 
 //Route files
 const hospitals = require('./routes/hospitals');
+const auth = require('./routes/auth');
 //Load env vars
 dotenv.config({path:'./config/config.env'});
 
@@ -15,9 +17,12 @@ const app = express();
 //Body parser
 app.use(express.json());
 
+//Cookie parser
+app.use(cookieParser()) ;
+
 //Mount routers
 app.use('/api/v1/hospitals', hospitals);
-
+app.use('/api/v1/auth',auth);
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, 'mode on port ', PORT));
 
